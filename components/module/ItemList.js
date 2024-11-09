fbe778cfe32e77070848f7302e8d1dda848da98e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-11-09 14:30:27 +0330

    i now in developing itemlist component

---
+++ components/module/ItemList.js
@@ -0,0 +1,19 @@
+const ItemList = ({ form, setForm }) => {
+  const { products } = form;
+
+  const addHandler = () => {
+    setForm({
+      ...form,
+      products: [...products, { name: "", price: "", qty: "" }],
+    });
+  };
+
+  return (
+    <div className="item-list">
+      <p>Purchased products</p>
+      <button onClick={addHandler}>Add Item</button>
+    </div>
+  );
+};
+
+export default ItemList;
