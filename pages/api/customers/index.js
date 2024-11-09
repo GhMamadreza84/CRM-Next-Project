d7eed9bc8158e4f652db79ed8132e2334a02bb10
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-11-09 08:53:05 +0330

    i handle the POST method in customer route in index.js file

--- pages/api/customers/index.js
+++ pages/api/customers/index.js
@@ -1,3 +1,4 @@
+import Customer from "../../../models/Customer";
 import connectDB from "../../../utils/connectDB";
 
 export default async function handler(req, res) {
@@ -9,5 +10,25 @@ export default async function handler(req, res) {
       .status(500)
       .json({ status: "failed", message: "Error in connecting to DB" });
     return;
+  }
+
+  if (req.method === "POST") {
+    const data = req.body.data;
+    if (!data.name || !data.lastName || !data.email) {
+      return res
+        .status(400)
+        .json({ status: "failed", message: "Invalid data" });
+    }
+    try {
+      const customer = await Customer.create(data);
+      res
+        .status(201)
+        .json({ status: "success", message: "Data created", data: customer });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ status: "failed", message: "Error in storing data in DB" });
+    }
   }
 }
