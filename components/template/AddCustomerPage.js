6e116902fcd000ecd857cd47f79b26a6afb0d36f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-11-09 09:50:57 +0330

    i now develope the addcustomer page ui now need develope the form in add customer page

--- components/template/AddCustomerPage.js
+++ components/template/AddCustomerPage.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import { useState } from "react";
+import Form from "../module/Form";
 
 const AddCustomerPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [form, setForm] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    address: "",
+    postalCode: "",
+    date: "",
+    products: [],
+  });
 
-export default AddCustomerPage
+  const cancelHandler = () => {};
+  const saveHandler = () => {};
+  return (
+    <div className="customer-page">
+      <h4>Add New Customer</h4>
+      <Form form={form} setForm={setForm} />
+      <div className="customer-page__buttons">
+        <button className="first" onClick={cancelHandler}>
+          Cancel
+        </button>
+        <button className="second" onClick={saveHandler}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddCustomerPage;
