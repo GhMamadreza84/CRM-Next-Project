d200cd12a4911a8fa1890485d2fb22ddf625f59f
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-11-09 08:40:38 +0330

    i now create the api folder in pages and create customer folder in api folder and write the trycatch block for connect to DB

---
+++ pages/api/customers/index.js
@@ -0,0 +1,13 @@
+import connectDB from "../../../utils/connectDB";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ status: "failed", message: "Error in connecting to DB" });
+    return;
+  }
+}
