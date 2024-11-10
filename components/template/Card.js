791bbc5e168ab09bf28076914d1bb87a88b6f764
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-11-10 13:23:39 +0330

    i now develope the card component now need develope the api route for delete function

--- components/template/Card.js
+++ components/template/Card.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-const Card = () => {
+const Card = ({ customer }) => {
+  const deleteHandler = () => {};
   return (
-    <div>
-      Card
+    <div className="card">
+      <div className="card__details">
+        <p>
+          {customer.name} {customer.lastName}
+        </p>
+        <p>{customer.email}</p>
+      </div>
+      <div className="card__buttons">
+        <button onClick={deleteHandler}>Delete</button>
+        <Link href={`/edit/${customer._id}`}>Edit</Link>
+        <Link href={`/customer/${customer._id}`}>Detailt</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
